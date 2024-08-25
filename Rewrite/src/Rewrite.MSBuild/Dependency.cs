ee49f14fdb2fd9bcb2220263411ea00525191eac
OlegDokuka <oleh@moderne.io>
2024-08-25 20:40:27 +0300

    Remove MSBuild project
    Signed-off-by: OlegDokuka <oleh@moderne.io>

--- Rewrite/src/Rewrite.MSBuild/Dependency.cs
+++
@@ -1,7 +0,0 @@
-using NuGet.Versioning;
-
-namespace Rewrite.MSBuild;
-
-public record Dependency(string Package, string Version)
-{
-}
--- Rewrite/src/Rewrite.MSBuild/Marker/ProjectDependencies.cs
+++
@@ -1,9 +0,0 @@
-namespace Rewrite.MSBuild.Marker;
-
-public record ProjectDependencies(Guid Id, string ProjectFile, IList<Dependency> Dependencies) : Core.Marker.Marker
-{
-    public bool Equals(Core.Marker.Marker? other)
-    {
-        return other is ProjectDependencies && other.Id.Equals(Id);
-    }
-}
