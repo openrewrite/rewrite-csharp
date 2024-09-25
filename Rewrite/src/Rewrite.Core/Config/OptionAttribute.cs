40638ce7ebba49b9ad10cffb4e2b35b11bfd4323
OlegDokuka <oleh@moderne.io>
2024-09-25 15:17:56 +0300

    Add support for option attr for recipe descriptor
    Signed-off-by: OlegDokuka <oleh@moderne.io>

--- Rewrite/src/Rewrite.Core/Config/OptionAttribute.cs
+++ Rewrite/src/Rewrite.Core/Config/OptionAttribute.cs
@@ -1,7 +1,13 @@
 namespace Rewrite.Core;
 
-[AttributeUsage(AttributeTargets.Field)]
-public sealed class OptionAttribute : Attribute
+[AttributeUsage(AttributeTargets.Parameter)]
+public sealed class OptionAttribute([LanguageInjection("markdown")] string displayName = "", [LanguageInjection("markdown")] string description = "", string example = "", params string[] valid) : Attribute
 {
-    
+    public string DisplayName => displayName;
+
+    public string Description => description;
+
+    public string Example => example;
+
+    public string[] Valid => valid;
 }
