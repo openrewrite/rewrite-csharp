0e7ef53e87ecf16574dcc3cd8154447ed02ac204
OlegDokuka <oleh@moderne.io>
2024-10-10 10:11:25 +0300

    add more fixes to CI

--- rewrite-test-engine-remote/build.gradle.kts
+++ rewrite-test-engine-remote/build.gradle.kts
@@ -51,6 +51,10 @@ tasks.named<Jar>("javadocJar") {
     enabled = false
 }
 
+
+tasks.withType<PublishToMavenRepository>().configureEach { enabled = false }
+tasks.withType<PublishToMavenLocal>().configureEach { enabled = false }
+
 val emptySourceJar = tasks.create<Jar>("emptySourceJar") {
     file("README.md")
     archiveClassifier.set("sources")
