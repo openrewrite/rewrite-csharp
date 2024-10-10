f68e9babcb9781ef3d8f5e6d7eac1d4d67a30398
Knut Wannheden <knut@moderne.io>
2024-10-10 12:16:30 +0200

    [lst-change] Correct namespace of `ModuleInitializer`

--- Rewrite/src/Rewrite.Remote.Codec/ModuleInitializer.cs
+++ Rewrite/src/Rewrite.Remote.Codec/ModuleInitializer.cs
@@ -1,20 +1,13 @@
 ﻿#pragma warning disable CA2255
 using System.Runtime.CompilerServices;
-using Rewrite.Remote;
 using Rewrite.Remote.Codec.CSharp;
 using Rewrite.Remote.Codec.Java;
 using Rewrite.Remote.Codec.Properties;
 using Rewrite.Remote.Codec.Xml;
 using Rewrite.Remote.Codec.Yaml;
 using Rewrite.RewriteCSharp.Tree;
-using Rewrite.RewriteJson.Tree;
-using Rewrite.RewriteProperties.Tree;
-using Rewrite.RewriteXml.Tree;
-using Rewrite.RewriteYaml.Tree;
-using JsonReceiver = Rewrite.Remote.Codec.Json.JsonReceiver;
-using JsonSender = Rewrite.Remote.Codec.Json.JsonSender;
 
-namespace Rewrite.CSharp.Tests;
+namespace Rewrite.Remote.Codec;
 
 public class ModuleInitializer
 {
@@ -24,13 +17,13 @@ public class ModuleInitializer
         Initialization.Initialize();
         SenderContext.Register(typeof(Cs), () => new CSharpSender());
         ReceiverContext.Register(typeof(Cs), () => new CSharpReceiver());
-        SenderContext.Register(typeof(Json), () => new JsonSender());
-        ReceiverContext.Register(typeof(Json), () => new JsonReceiver());
-        SenderContext.Register(typeof(Xml), () => new XmlSender());
-        ReceiverContext.Register(typeof(Xml), () => new XmlReceiver());
-        SenderContext.Register(typeof(Yaml), () => new YamlSender());
-        ReceiverContext.Register(typeof(Yaml), () => new YamlReceiver());
-        SenderContext.Register(typeof(Properties), () => new PropertiesSender());
-        ReceiverContext.Register(typeof(Properties), () => new PropertiesReceiver());
+        SenderContext.Register(typeof(RewriteJson.Tree.Json), () => new Json.JsonSender());
+        ReceiverContext.Register(typeof(RewriteJson.Tree.Json), () => new Json.JsonReceiver());
+        SenderContext.Register(typeof(RewriteXml.Tree.Xml), () => new XmlSender());
+        ReceiverContext.Register(typeof(RewriteXml.Tree.Xml), () => new XmlReceiver());
+        SenderContext.Register(typeof(RewriteYaml.Tree.Yaml), () => new YamlSender());
+        ReceiverContext.Register(typeof(RewriteYaml.Tree.Yaml), () => new YamlReceiver());
+        SenderContext.Register(typeof(RewriteProperties.Tree.Properties), () => new PropertiesSender());
+        ReceiverContext.Register(typeof(RewriteProperties.Tree.Properties), () => new PropertiesReceiver());
     }
 }
