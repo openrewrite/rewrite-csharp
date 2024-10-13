9da3574220185c5090b4a000c06eeed0275a3335
Andrew Stakhov <andrew.stakhov@gmail.com>
2024-10-13 01:41:26 -0400

    Improved string delta rendering for tests

--- Rewrite/tests/Rewrite.CSharp.Tests/StringAssertionExtensions.cs
+++ Rewrite/tests/Rewrite.CSharp.Tests/StringAssertionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using DiffPlex;
 using DiffPlex.Chunkers;
 using DiffPlex.DiffBuilder.Model;
@@ -19,44 +20,82 @@ public static class StringAssertionExtensions
     }
     public static string? GetDifferences(string oldText, string newText)
     {
+        string AddVisibleCrLf(string str) =>
+            Regex.Replace(str.Replace("\r", "\u240D").Replace("\n", "\u2424"), "([\u240D\u2424]+)", "$1\n");// + (AnsiColor.ColorizeText("<-EOF", AnsiColor.Foreground(Terminal256ColorCodes.Yellow1C226)));
+        oldText = AddVisibleCrLf(oldText);
+        newText = AddVisibleCrLf(newText);
 
         var differ = new Differ();
-        var diff = DiffPlex.DiffBuilder.SideBySideDiffBuilder.Diff(differ, oldText, newText, wordChunker: new CharacterChunker());
+        var diff = DiffPlex.DiffBuilder.SideBySideDiffBuilder.Diff(differ, oldText, newText, ignoreWhiteSpace: false, wordChunker: new CharacterChunker());
         if (!diff.NewText.HasDifferences)
             return null;
-        int padSize = diff.OldText.Lines.Count > 0 ? (int)Math.Log10(diff.OldText.Lines.Count) : 0;
+        int padSize = diff.OldText.Lines.Count > 0 ? (int)Math.Log10(diff.OldText.Lines.Count) + 1 : 1;
 
         var result = new StringBuilder();
-
+        var oldLastLineIndex = diff.OldText.Lines.Last(x => x.Type != ChangeType.Imaginary).Position;
+        var newLastLineIndex = diff.NewText.Lines.Last(x => x.Type != ChangeType.Imaginary).Position;
         for (int i = 0; i < diff.OldText.Lines.Count ; i++)
         {
-            var leftLine = diff.OldText.Lines[i];
-            var rightLine = diff.NewText.Lines[i];
+            var oldLine = diff.OldText.Lines[i];
+            var newLine = diff.NewText.Lines[i];
             var lineChange = diff.NewText.Lines[i].Type;
             if (lineChange == ChangeType.Unchanged)
                 continue;
             result.AppendLine("-------------------");
-            string leftRender = AnsiColor.ColorizeText(leftLine.Text, AnsiColor.Background(Terminal256ColorCodes.BlackC0))!;
-            string rightRender = AnsiColor.ColorizeText(rightLine.Text, AnsiColor.Background(Terminal256ColorCodes.BlackC0))!;
-            if (lineChange == ChangeType.Modified)
+            string oldRender = AnsiColor.ColorizeText(oldLine.Text, AnsiColor.Background(Terminal256ColorCodes.BlackC0))!;
+            string newRender = AnsiColor.ColorizeText(newLine.Text, AnsiColor.Background(Terminal256ColorCodes.BlackC0))!;
+            if (oldLine.Text != null && newLine.Text != null)
             {
-                var leftChanges = diff.OldText.Lines[i].SubPieces;
-                var rightChanges = diff.NewText.Lines[i].SubPieces;
-                leftRender = GetDiffText(leftChanges);
-                rightRender = GetDiffText(rightChanges);
+                // check if this is a pure linebreak issue. if it is, collapse diffs for this line and next, into a single diff rendering (we just need to highlight the missing line break characters)
+                var oldThisAndNextLine = diff.OldText.Lines.Count > i + 1 ? oldLine.Text + diff.OldText.Lines[i+1].Text : oldLine.Text;
+                var newThisAndNextLine = diff.NewText.Lines.Count > i + 1 ? newLine.Text + diff.NewText.Lines[i+1].Text : newLine.Text;
+                string StripWhitespace(string str) => Regex.Replace(str,"[\u240D\u2424\n]","");
+                if(StripWhitespace(oldThisAndNextLine) == StripWhitespace(newThisAndNextLine))
+                {
+                    var twoLineDiff = DiffPlex.DiffBuilder.SideBySideDiffBuilder.Diff(differ, oldThisAndNextLine, newThisAndNextLine, ignoreWhiteSpace: false, wordChunker: new CharacterChunker());
+                    oldLine = twoLineDiff.OldText.Lines[0];
+                    newLine = twoLineDiff.NewText.Lines[0];
+                    i++;
+                }
             }
 
-            var lineNum = i + 1;
-            // var right = GetDiffText);
-            var leftLineNum = $"{lineNum.ToString()!.PadRight(padSize)}" ;
-            var rightLineNum = leftLineNum;
 
-            leftLineNum = AnsiColor.ColorizeText(leftLineNum, GetStyle(leftLine.Type)) + "|";
-            rightLineNum = AnsiColor.ColorizeText(rightLineNum, GetStyle(rightLine.Type)) + "|";
+            if (lineChange == ChangeType.Modified)
+            {
+                oldRender = GetDiffText(oldLine.SubPieces);
+                newRender = GetDiffText(newLine.SubPieces);
+            }
 
+            var oldLineNum = oldLine.Position?.ToString();
+            var newLineNum = newLine.Position?.ToString();
 
-            result.AppendLine($"{leftLineNum}{leftRender}");
-            result.AppendLine($"{rightLineNum}{rightRender}");
+            oldLineNum = $"{AnsiColor.ColorizeText(oldLineNum, GetStyle(oldLine.Type))}|".PadLeft(padSize);
+            newLineNum = $"{AnsiColor.ColorizeText(newLineNum, GetStyle(newLine.Type))}|".PadLeft(padSize);
+
+            // if whole line is different, then make every character take line colour
+            if (oldLine.Type is not ChangeType.Modified)
+            {
+                oldRender = AnsiColor.ColorizeText(oldLine.Text, GetStyle(oldLine.Type))!;
+            }
+            if (newLine.Type is not ChangeType.Modified)
+            {
+                newRender = AnsiColor.ColorizeText(newLine.Text!, GetStyle(newLine.Type))!;
+            }
+            // append EOF character
+            if (oldLine.Position == oldLastLineIndex)
+            {
+                oldRender += AnsiColor.ColorizeText("\u2403", AnsiColor.Foreground(Terminal256ColorCodes.YellowC11));
+            }
+            if (newLine.Position == oldLastLineIndex)
+            {
+                newRender += AnsiColor.ColorizeText("\u2403", AnsiColor.Foreground(Terminal256ColorCodes.YellowC11));
+            }
+
+            if (oldLine.Type is not ChangeType.Imaginary)
+                result.AppendLine($"{oldLineNum}{oldRender}");
+
+            if (newLine.Type is not ChangeType.Imaginary)
+                result.AppendLine($"{newLineNum}{newRender}");
         }
 
         return result.ToString();
@@ -68,8 +107,11 @@ public static class StringAssertionExtensions
         foreach (var charDelta in current)
         {
             var text = charDelta.Text;
-            var style = AnsiColor.Composite(GetStyle(charDelta.Type),
-                AnsiColor.Background(Terminal256ColorCodes.BlackC0));
+            if (charDelta.Type == ChangeType.Imaginary)
+            {
+                text = " ";
+            }
+            var style = GetStyle(charDelta.Type);
             sb.Append(AnsiColor.ColorizeText(text, style));
         }
 
@@ -80,11 +122,12 @@ public static class StringAssertionExtensions
     {
         return type switch
         {
-            ChangeType.Deleted => AnsiColor.Foreground(Terminal256ColorCodes.Red1C196),
-            ChangeType.Inserted => AnsiColor.Foreground(Terminal256ColorCodes.Green1C46),
-            ChangeType.Modified => AnsiColor.Background(Terminal256ColorCodes.Blue1C21),
-            ChangeType.Imaginary => AnsiColor.Composite(AnsiColor.Background(Terminal256ColorCodes.GreyC8), AnsiColor.Foreground(Terminal256ColorCodes.SeaGreen3C78)),
-            _ => AnsiColor.Reset
+            ChangeType.Deleted => AnsiColor.Composite(AnsiColor.Foreground(Terminal256ColorCodes.Red1C196), AnsiColor.Background(Terminal256ColorCodes.BlackC0)),
+            ChangeType.Inserted => AnsiColor.Composite(AnsiColor.Foreground(Terminal256ColorCodes.Green1C46), AnsiColor.Background(Terminal256ColorCodes.BlackC0)),
+            ChangeType.Modified => AnsiColor.Composite(AnsiColor.Background(Terminal256ColorCodes.Blue1C21)),
+            ChangeType.Imaginary => AnsiColor.Reset, // AnsiColor.Background(Terminal256ColorCodes.GreyC8),
+            // ChangeType.Imaginary => AnsiColor.Composite(AnsiColor.Background(Terminal256ColorCodes.GreyC8), AnsiColor.Foreground(Terminal256ColorCodes.SeaGreen3C78)),
+            _ => AnsiColor.Background(Terminal256ColorCodes.BlackC0)
         };
     }
 }
