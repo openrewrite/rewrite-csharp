d2c5eb4da0dc29f112675e2e883878cd5a59953d
Knut Wannheden <knut@moderne.io>
2024-12-30 20:17:31 +0100

    Align `JavaType` with recent OpenRewrite changes (#29)
    * Align `JavaType` with recent OpenRewrite changes
    
    See:
     - openrewrite/rewrite#4813
     - openrewrite/rewrite#4814
    
    * Add missing change

--- Rewrite/src/Rewrite.Java/Tree/JavaType.cs
+++ Rewrite/src/Rewrite.Java/Tree/JavaType.cs
@@ -9,10 +9,11 @@ namespace Rewrite.RewriteJava.Tree;
 [SuppressMessage("ReSharper", "PossibleUnintendedReferenceComparison")]
 public interface JavaType
 {
-    internal static JavaType[] EMPTY_JAVA_TYPE_ARRAY = [];
-    internal static FullyQualified[] EMPTY_FULLY_QUALIFIED_ARRAY = [];
-    internal static Method[] EMPTY_METHOD_ARRAY = [];
-    internal static Variable[] EMPTY_VARIABLE_ARRAY = [];
+    internal static readonly JavaType[] EMPTY_JAVA_TYPE_ARRAY = [];
+    internal static readonly FullyQualified[] EMPTY_FULLY_QUALIFIED_ARRAY = [];
+    internal static readonly Method[] EMPTY_METHOD_ARRAY = [];
+    internal static readonly Variable[] EMPTY_VARIABLE_ARRAY = [];
+    internal static readonly string[] EMPTY_STRING_ARRAY = [];
 
     internal int? ManagedReference => null;
 
@@ -324,7 +325,7 @@ public interface JavaType
             IList<FullyQualified>? annotations, IList<FullyQualified>? interfaces,
             IList<Variable>? members, IList<Method>? methods)
         {
-            
+
             TypeParameters = typeParameters ?? EMPTY_JAVA_TYPE_ARRAY;
             Supertype = supertype;
             OwningClass = owningClass;
@@ -531,9 +532,10 @@ public interface JavaType
             JavaType? returnType,
             IList<string>? parameterNames,
             IList<JavaType>? parameterTypes,
-            IList<FullyQualified>? thrownExceptions,
+            IList<JavaType>? thrownExceptions,
             IList<FullyQualified>? annotations,
-            IList<string>? defaultValue)
+            IList<string>? defaultValue,
+            IList<string>? declaredFormalTypeNames)
         {
             ManagedReference = managedReference;
             FlagsBitMap = flagsBitMap;
@@ -542,9 +544,10 @@ public interface JavaType
             ReturnType = returnType ?? Unknown.Instance;
             ParameterNames = parameterNames ?? (Enumerable.Empty<string>() as IList<string>)!;
             ParameterTypes = parameterTypes ?? EMPTY_JAVA_TYPE_ARRAY;
-            ThrownExceptions = thrownExceptions ?? EMPTY_FULLY_QUALIFIED_ARRAY;
+            ThrownExceptions = thrownExceptions ?? EMPTY_JAVA_TYPE_ARRAY;
             Annotations = annotations ?? EMPTY_FULLY_QUALIFIED_ARRAY;
             DefaultValue = defaultValue;
+            DeclaredFormalTypeNames = declaredFormalTypeNames ?? EMPTY_STRING_ARRAY;
         }
 
         public int? ManagedReference { get; internal set; }
@@ -573,11 +576,13 @@ public interface JavaType
             return "<constructor>".Equals(Name);
         }
 
-        public IList<FullyQualified> ThrownExceptions { get; set; } = [];
+        public IList<JavaType> ThrownExceptions { get; set; } = [];
 
         public IList<FullyQualified> Annotations { get; internal set; } = [];
 
         public IList<string>? DefaultValue { get; internal set; } = [];
+
+        public IList<string>? DeclaredFormalTypeNames { get; internal set; } = [];
     }
 
     public sealed class Variable : JavaType
