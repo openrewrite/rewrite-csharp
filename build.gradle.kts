b689988f7a7703d9871fd41608fff0d8c2342ac8
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-04-08 17:41:04 +0200

    add license plugin and apply license headers

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("org.openrewrite.build.root") version("latest.release")
     id("org.openrewrite.build.java-base") version("latest.release")
     id("org.owasp.dependencycheck") version "latest.release"
+    id("org.openrewrite.build.moderne-source-available-license") version "latest.release"
 }
 
 allprojects {
