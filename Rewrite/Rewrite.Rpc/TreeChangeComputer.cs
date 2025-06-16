baa270b290595e738a062c8421545ef2b4540edc
Andrew Stakhov <andrew.stakhov@gmail.com>
2025-06-16 16:53:55 -0400

    Delta serializer work

--- Rewrite/Rewrite.Rpc/TreeChangeComputer.cs
+++ Rewrite/Rewrite.Rpc/TreeChangeComputer.cs
@@ -1,27 +1,27 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Rewrite.Rpc;
-
-public class TreeChangeComputer
-{
-    public List<RpcObjectData> GetChanges(object before, object after)
-    {
-        var result = new List<RpcObjectData>();
-        if (before == after)
-        {
-            result.Add(new RpcObjectData { State = RpcObjectData.ObjectState.NO_CHANGE });
-        }
-        // else if (beforeVal == null) {
-    }
-}
-
-public class SessionCache
-{
-    private readonly Dictionary<string, object?> _remoteObjects = new();
-    private readonly Dictionary<string, object> _localObjects = new();
-    // private readonly Dictionary<string, Recipe> _preparedRecipes = new();
-    public void Test()
-    {
-        _localObjects.GetValue()
-    }
-}
+﻿// using System.Runtime.CompilerServices;
+//
+// namespace Rewrite.Rpc;
+//
+// public class TreeChangeComputer
+// {
+//     public List<RpcObjectData> GetChanges(object before, object after)
+//     {
+//         var result = new List<RpcObjectData>();
+//         if (before == after)
+//         {
+//             result.Add(new RpcObjectData { State = RpcObjectData.ObjectState.NO_CHANGE });
+//         }
+//         // else if (beforeVal == null) {
+//     }
+// }
+//
+// public class SessionCache
+// {
+//     private readonly Dictionary<string, object?> _remoteObjects = new();
+//     private readonly Dictionary<string, object> _localObjects = new();
+//     // private readonly Dictionary<string, Recipe> _preparedRecipes = new();
+//     public void Test()
+//     {
+//         _localObjects.GetValue()
+//     }
+// }
